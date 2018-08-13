6feb6ac1672f8124577e6eb6e2c18d921f19ea8d
Rohan W <rohanxtreme@hotmail.com>
2018-08-13 19:44:31 +0530

    Add project files.

---
+++ AzureVMApp/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace AzureVMApp
+{
+    partial class frmVMManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVMStatus = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbResourceGroup = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstVM = new System.Windows.Forms.ListBox();
+            this.btnDeallocate = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstBoxLog = new System.Windows.Forms.ListBox();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Action : ";
+            // 
+            // lblVMStatus
+            // 
+            this.lblVMStatus.AutoSize = true;
+            this.lblVMStatus.Location = new System.Drawing.Point(104, 229);
+            this.lblVMStatus.Name = "lblVMStatus";
+            this.lblVMStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblVMStatus.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Green;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(117, 182);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 30);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStop.Location = new System.Drawing.Point(201, 182);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(81, 30);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Power Off";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resource Groups : ";
+            // 
+            // cmbResourceGroup
+            // 
+            this.cmbResourceGroup.FormattingEnabled = true;
+            this.cmbResourceGroup.Location = new System.Drawing.Point(117, 22);
+            this.cmbResourceGroup.Name = "cmbResourceGroup";
+            this.cmbResourceGroup.Size = new System.Drawing.Size(358, 21);
+            this.cmbResourceGroup.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Virtual Machines:";
+            // 
+            // lstVM
+            // 
+            this.lstVM.FormattingEnabled = true;
+            this.lstVM.Location = new System.Drawing.Point(117, 59);
+            this.lstVM.Name = "lstVM";
+            this.lstVM.Size = new System.Drawing.Size(358, 108);
+            this.lstVM.TabIndex = 7;
+            this.lstVM.SelectedIndexChanged += new System.EventHandler(this.lstVM_SelectedIndexChanged);
+            // 
+            // btnDeallocate
+            // 
+            this.btnDeallocate.BackColor = System.Drawing.Color.DarkRed;
+            this.btnDeallocate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeallocate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDeallocate.Location = new System.Drawing.Point(285, 182);
+            this.btnDeallocate.Name = "btnDeallocate";
+            this.btnDeallocate.Size = new System.Drawing.Size(81, 30);
+            this.btnDeallocate.TabIndex = 8;
+            this.btnDeallocate.Text = "Deallocate";
+            this.btnDeallocate.UseVisualStyleBackColor = false;
+            this.btnDeallocate.Click += new System.EventHandler(this.btnDeallocate_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 390);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(487, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Log :";
+            // 
+            // lstBoxLog
+            // 
+            this.lstBoxLog.FormattingEnabled = true;
+            this.lstBoxLog.HorizontalScrollbar = true;
+            this.lstBoxLog.Location = new System.Drawing.Point(117, 229);
+            this.lstBoxLog.Name = "lstBoxLog";
+            this.lstBoxLog.ScrollAlwaysVisible = true;
+            this.lstBoxLog.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lstBoxLog.Size = new System.Drawing.Size(358, 147);
+            this.lstBoxLog.TabIndex = 11;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(112, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel";
+            // 
+            // frmVMManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 412);
+            this.Controls.Add(this.lstBoxLog);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnDeallocate);
+            this.Controls.Add(this.lstVM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbResourceGroup);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblVMStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "frmVMManager";
+            this.Text = "Azure VM Manager";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVMStatus;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbResourceGroup;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstVM;
+        private System.Windows.Forms.Button btnDeallocate;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstBoxLog;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ AzureVMApp/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace AzureVMApp
+{
+    public partial class frmVMManager : Form
+    {
+        Microsoft.Azure.Management.ResourceManager.Fluent.Authentication.AzureCredentials credentials = null;
+        Microsoft.Azure.Management.Fluent.Azure azure;
+
+        string _groupName = string.Empty;
+        
+        public frmVMManager()
+        {
+            InitializeComponent();
+
+            try
+            {
+                toolStripStatusLabel1.Text = "Attempting to get Azure config file";
+
+                credentials = SdkContext.AzureCredentialsFactory.FromFile(@"azureauth.properties");
+
+                if (credentials != null)
+                {
+                    lstBoxLog.Items.Add(toolStripStatusLabel1.Text);
+
+                    azure = (Azure)Azure
+                        .Configure()
+                        .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                        .Authenticate(credentials)
+                        .WithDefaultSubscription();
+
+                    FillResourceGroups();
+                    toolStripStatusLabel1.Text = "";
+
+                    btnDeallocate.BackColor = Color.Gray;
+                    btnStart.BackColor = Color.Gray;
+                    btnStop.BackColor = Color.Gray;
+                }
+                else
+                {
+                    MessageBox.Show("Error in retreiving AzureAuth File. Please review the pre-requisites in README and try again");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+           
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lstBoxLog.Items.Add("Starting VM");
+                string ipAddress = string.Empty;
+                foreach (KeyValuePair<string, string> item in lstVM.SelectedItems)
+                {
+                    var vm = azure.VirtualMachines.GetByResourceGroup(_groupName, item.Key);
+                    PowerOnVM(vm);                   
+                }
+
+                lstBoxLog.Items.Add("VM Start operation initiated");
+                MessageBox.Show("VM Start initated");                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void PowerOnVM(IVirtualMachine virtualMachine)
+        {
+            string vmName = virtualMachine.ComputerName;
+            Task task = virtualMachine.StartAsync();
+            await task;
+            lstBoxLog.Items.Add(vmName + " Started at " + DateTime.Now);
+            lstBoxLog.Items.Add(vmName + " IPAddress : " + virtualMachine.GetPrimaryPublicIPAddress().IPAddress);
+            UpdateVMData(null, null);
+        }
+
+
+
+        private void ShowMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (KeyValuePair<string, string> item in lstVM.SelectedItems)
+                {
+                    lstBoxLog.Items.Add("Powering off VM : " + item);
+                    var vm = azure.VirtualMachines.GetByResourceGroup(_groupName, item.Key);
+                    PowerOffVM(vm);
+                    lstBoxLog.Items.Add(item + " VM PoweredOff: ");
+                }
+                
+                MessageBox.Show("VM Stopped");                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void PowerOffVM(IVirtualMachine virtualMachine)
+        {
+            string vmName = virtualMachine.ComputerName;
+            Task task = virtualMachine.PowerOffAsync();
+            await task;
+            lstBoxLog.Items.Add(vmName + "stopped at " + DateTime.Now);
+            UpdateVMData(null, null);
+        }
+
+        private void FillResourceGroups()
+        {
+            toolStripStatusLabel1.Text = "Filling Resource Group Information";
+            var rGroup = azure.ResourceGroups.List();
+            
+
+            foreach (var item in rGroup)
+            {
+                cmbResourceGroup.Items.Add(
+                    item.Name
+                    );                
+            }
+
+            cmbResourceGroup.SelectedIndex = 0;
+
+            cmbResourceGroup.SelectedIndexChanged += UpdateVMData;
+        }
+                
+        private void UpdateVMData(object sender, EventArgs e)
+        {
+            //lstVM.Items.Clear();
+            toolStripStatusLabel1.Text = "Filling VM Details for ResourceGroup " + _groupName;
+
+            _groupName = cmbResourceGroup.SelectedItem.ToString();
+
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            foreach (var item in azure.VirtualMachines.ListByResourceGroup(_groupName))
+            {
+                lstBoxLog.Items.Add("Getting VM Status for " + item.ComputerName);
+                foreach (InstanceViewStatus stat in item.InstanceView.Statuses)
+                {
+                    string[] statusData = stat.Code.Split('/');
+                    if (statusData[0].Equals("PowerState"))
+                    {
+                        list.Add(item.ComputerName, statusData[1]);
+                    }                   
+                }                                
+            }
+
+            lstVM.DataSource = new BindingSource(list, null); 
+            lstVM.DisplayMember = "Key";
+            lstVM.ValueMember = "Value";
+
+            /*(lstVM.Items.Add(
+                    item.ComputerName
+                   );*/
+            toolStripStatusLabel1.Text = "";
+
+
+        }
+
+        private void lstVM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+            btnDeallocate.Enabled = false;
+
+            btnDeallocate.BackColor = Color.Gray;
+            btnStart.BackColor = Color.Gray;
+            btnStop.BackColor = Color.Gray;
+
+            var state = (KeyValuePair<string,string>)lstVM.SelectedItem;
+
+            if (state.Value.Equals("running"))
+            {
+                btnStop.Enabled = true;
+                btnStop.BackColor = Color.DarkRed;
+                btnDeallocate.Enabled = true;
+                btnDeallocate.BackColor = Color.DarkOrange;
+            }
+            else if(state.Value.Equals("stopped") || state.Value.Equals("deallocated"))
+            {
+                btnStart.Enabled = true;
+                btnStart.BackColor = Color.Green;
+            }
+        }
+
+        private void btnDeallocate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (KeyValuePair<string, string> item in lstVM.SelectedItems)
+                {
+                    lstBoxLog.Items.Add("Deallocating VM : " + item.Key);
+                    var vm = azure.VirtualMachines.GetByResourceGroup(_groupName, item.Key);
+                    DeallocateVM(vm);
+                    lstBoxLog.Items.Add(item.Key + " VM Deallocated");
+                }
+
+                MessageBox.Show("VM Deallocated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void DeallocateVM(IVirtualMachine virtualMachine)
+        {
+            string vmName = virtualMachine.ComputerName;
+            Task task = virtualMachine.DeallocateAsync();
+            await task;
+            lstBoxLog.Items.Add(vmName + " deallocated at " + DateTime.Now);
+            UpdateVMData(null, null);
+        }
+    }
+
+}
